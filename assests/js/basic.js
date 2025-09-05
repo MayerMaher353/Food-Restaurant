aaa6c38e804af204a63678d9d0a5e0625de75bce
khadeejasherif <khadeejasherif372@gmail.com>
2025-09-05 13:05:52 +0300

    Replace old code with new code

--- assests/js/basic.js
+++ assests/js/basic.js
@@ -1,7 +1,75 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,       // default = 3 slides
+    spaceBetween: 30,
+    loop: true, // Makes it infinite loop
+    centeredSlides:true,
+    speed:1200, 
+    threshold:15,
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true, // Makes dots clickable
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        0: { slidesPerView: 1 },      // mobile
+        992: { slidesPerView: 3 }     // laptop and above
+    },
+    on: {
+     slideChangeTransitionStart: function () {
+       document.querySelectorAll('.quotes').forEach(q => {
+          q.classList.remove('quotes-animate');
+       });
+       const activeQuote = swiper.slides[swiper.activeIndex].querySelector('.quotes');
+        if (activeQuote) {
+          activeQuote.classList.add('quotes-animate');
+        }
+     },
+     slideChange: function () {
+          document.querySelectorAll('.swiper-pagination-bullet').forEach(b => {
+            b.classList.remove('pulse');
+          });
+
+          const active = document.querySelector('.swiper-pagination-bullet-active');
+          if (active) {
+            void active.offsetWidth;
+            active.classList.add('pulse');
+          }
+     }
+    },
+});
+const slides = document.querySelectorAll('.swiper-slide');
+slides.forEach(slide => {
+  const clone = slide.cloneNode(true);
+  document.querySelector('.swiper-wrapper').appendChild(clone);
+});
+function animateCounter(id, target, duration) {
+    const counter = document.getElementById(id);
+    let start = 0;
+    const stepTime = Math.abs(Math.floor(duration / target));
+
+    const timer = setInterval(() => {
+      start += 1;
+      counter.textContent = start;
+      if (start >= target) {
+        clearInterval(timer);
+      }
+    }, stepTime);
+}
+animateCounter("counter1", 200, 2000);
+animateCounter("counter2", 400, 2000);
+animateCounter("counter3", 100, 2000);
+animateCounter("counter4", 40, 2000);
 
 const gallerySlider = new Swiper(".gallery-container", {
   loop: true,
-  slidesPerView: "auto",   //  auto respects fixed CSS
+  slidesPerView: "auto",   // 👈 auto respects fixed CSS
   spaceBetween: 13,
   navigation: {
     nextEl: ".gallery-next",
