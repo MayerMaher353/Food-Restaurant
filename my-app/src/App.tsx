352c90eeb4e0026a31b9648da1755936877e9ec2
Rania Raafat <raniaraafat421@gmail.com>
2025-10-31 21:58:41 +0200

    add new components

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Footer from './shared/footer/footer'
+import WorkingHours from './shared/WorkingHours/WorkingHours'
+import Testemonials from './shared/Testemonials/Testemonials'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+     <WorkingHours />
+     <Testemonials />
      {/* Footer appears here */}
       <Footer />
     </>
